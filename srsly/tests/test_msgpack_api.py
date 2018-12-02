3a12d3c49a0045e7636bb9b84a173ea61022fb02
Ines Montani <ines@ines.io>
2018-12-02 17:49:48 +0100

    Add expected options to prevent error on Python 2
    (due to difference in key sorting)

--- srsly/tests/test_msgpack_api.py
+++ srsly/tests/test_msgpack_api.py
@@ -25,8 +25,9 @@ def make_tempdir(files={}):
 
 def test_msgpack_dumps():
     data = {"hello": "world", "test": 123}
+    expected = [b"\x82\xa5hello\xa5world\xa4test{", b"\x82\xa4test{\xa5hello\xa5world"]
     msg = msgpack_dumps(data)
-    assert msg == b"\x82\xa5hello\xa5world\xa4test{"
+    assert msg in expected
 
 
 def test_msgpack_loads():
@@ -58,8 +59,9 @@ def test_read_msgpack_file_invalid():
 
 def test_write_msgpack_file():
     data = {"hello": "world", "test": 123}
+    expected = [b"\x82\xa5hello\xa5world\xa4test{", b"\x82\xa4test{\xa5hello\xa5world"]
     with make_tempdir() as temp_dir:
         file_path = temp_dir / "tmp.msg"
         write_msgpack(file_path, data)
         with Path(file_path).open("rb") as f:
-            assert f.read() == b"\x82\xa5hello\xa5world\xa4test{"
+            assert f.read() in expected
