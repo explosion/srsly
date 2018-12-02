93d6b5e33747169a102629015a63c1d4006cf81e
Matthew Honnibal <honnibal+gh@gmail.com>
2018-12-02 04:52:11 +0100

    Revert "Set version to v0.0.4"
    This reverts commit 5d4bfd8e92db765cdf4d7f8948b2293ca73aa245.

--- srsly/about.py
+++ srsly/about.py
@@ -1,5 +1,5 @@
 __title__ = "srsly"
-__version__ = "0.0.4"
+__version__ = "0.0.3"
 __summary__ = "Modern high-performance serialization utilities for Python"
 __uri__ = "https://explosion.ai"
 __author__ = "Explosion AI"
