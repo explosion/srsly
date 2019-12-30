beaa73e5192540f6492b67a674275de83e0728ef
Ines Montani <ines@ines.io>
2019-12-30 20:51:03 +0100

    Add setup.cfg and modernize setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,22 @@
 #!/usr/bin/env python
-from __future__ import print_function
-
-import io
 import os
 import subprocess
 import sys
 import contextlib
-from glob import glob
 from distutils.command.build_ext import build_ext
 from distutils.sysconfig import get_python_inc
-from distutils import ccompiler, msvccompiler
 from setuptools import Extension, setup, find_packages
+from pathlib import Path
 
 
 PACKAGE_DATA = {"": ["*.pyx", "*.pxd", "*.c", "*.h"]}
-
-
 PACKAGES = find_packages()
-
-
 MOD_NAMES = ["srsly.msgpack._unpacker", "srsly.msgpack._packer"]
-
-
 COMPILE_OPTIONS = {
     "msvc": ["/Ox", "/EHsc"],
     "mingw32": ["-O2", "-Wno-strict-prototypes", "-Wno-unused-function"],
     "other": ["-O2", "-Wno-strict-prototypes", "-Wno-unused-function"],
 }
-
-
 LINK_OPTIONS = {"msvc": [], "mingw32": [], "other": ["-lstdc++", "-lm"]}
 
 if sys.byteorder == "big":
@@ -37,7 +25,8 @@ else:
     macros = [("__LITTLE_ENDIAN__", "1")]
 
 
-# By subclassing build_extensions we have the actual compiler that will be used which is really known only after finalize_options
+# By subclassing build_extensions we have the actual compiler that will be used
+# which is really known only after finalize_options
 # http://stackoverflow.com/questions/724664/python-distutils-how-to-get-a-compiler-that-is-going-to-be-used
 class build_ext_options:
     def build_options(self):
@@ -59,25 +48,19 @@ class build_ext_subclass(build_ext, build_ext_options):
 
 def generate_cython(root, source):
     print("Cythonizing sources")
-    p = subprocess.call(
-        [sys.executable, os.path.join(root, "bin", "cythonize.py"), source],
-        env=os.environ,
-    )
+    script = root / "bin" / "cythonize.py"
+    p = subprocess.call([sys.executable, str(script), source], env=os.environ)
     if p != 0:
         raise RuntimeError("Running cythonize failed")
-
-
-def is_source_release(path):
-    return os.path.exists(os.path.join(path, "PKG-INFO"))
 
 
 def clean(path):
     for name in MOD_NAMES:
         name = name.replace(".", "/")
-        for ext in [".so", ".html", ".cpp", ".c"]:
-            file_path = os.path.join(path, name + ext)
-            if os.path.exists(file_path):
-                os.unlink(file_path)
+        for ext in ["so", "html", "cpp", "c"]:
+            file_path = path / f"{name}.{ext}"
+            if file_path.exists():
+                file_path.unlink()
 
 
 @contextlib.contextmanager
@@ -93,24 +76,17 @@ def chdir(new_dir):
 
 
 def setup_package():
-    root = os.path.abspath(os.path.dirname(__file__))
+    root = Path(__file__).parent
 
     if len(sys.argv) > 1 and sys.argv[1] == "clean":
         return clean(root)
 
     with chdir(root):
-        with io.open(os.path.join(root, "srsly", "about.py"), encoding="utf8") as f:
+        with (root / "srsly" / "about.py").open("r") as f:
             about = {}
             exec(f.read(), about)
 
-        with io.open(os.path.join(root, "README.md"), encoding="utf8") as f:
-            readme = f.read()
-
-        include_dirs = [
-            get_python_inc(plat_specific=True),
-            ".",
-            os.path.join(root, "include"),
-        ]
+        include_dirs = [get_python_inc(plat_specific=True), ".", root / "include"]
 
         ext_modules = []
         for mod_name in MOD_NAMES:
@@ -152,45 +128,15 @@ def setup_package():
             )
         )
 
-        if not is_source_release(root):
+        if not (root / "PKG-INFO").exists():  # not source release
             generate_cython(root, "srsly")
 
         setup(
             name="srsly",
-            zip_safe=True,
             packages=PACKAGES,
             package_data=PACKAGE_DATA,
-            description=about["__summary__"],
-            long_description=readme,
-            long_description_content_type="text/markdown",
-            author=about["__author__"],
-            author_email=about["__email__"],
             version=about["__version__"],
-            url=about["__uri__"],
-            license=about["__license__"],
             ext_modules=ext_modules,
-            setup_requires=[],
-            install_requires=['pathlib==1.0.1; python_version < "3.4"'],
-            classifiers=[
-                "Development Status :: 5 - Production/Stable",
-                "Environment :: Console",
-                "Intended Audience :: Developers",
-                "Intended Audience :: Science/Research",
-                "License :: OSI Approved :: MIT License",
-                "Operating System :: POSIX :: Linux",
-                "Operating System :: MacOS :: MacOS X",
-                "Operating System :: Microsoft :: Windows",
-                "Programming Language :: Cython",
-                "Programming Language :: Python :: 2",
-                "Programming Language :: Python :: 2.7",
-                "Programming Language :: Python :: 3",
-                "Programming Language :: Python :: 3.4",
-                "Programming Language :: Python :: 3.5",
-                "Programming Language :: Python :: 3.6",
-                "Programming Language :: Python :: 3.7",
-                "Programming Language :: Python :: 3.8",
-                "Topic :: Scientific/Engineering",
-            ],
             cmdclass={"build_ext": build_ext_subclass},
         )
 
