3c99cbe1163fe357dcadbddc9c3d43b7216e4b91
Ines Montani <ines@ines.io>
2019-12-30 20:51:20 +0100

    Tidy up __init__.py

--- srsly/__init__.py
+++ srsly/__init__.py
@@ -1,7 +1,9 @@
 # coding: utf8
 from __future__ import unicode_literals
 
-from ._json_api import read_json, read_gzip_json, write_json, write_gzip_json, read_jsonl, write_jsonl
+from ._json_api import read_json, read_gzip_json, write_json, write_gzip_json
+from ._json_api import read_jsonl, write_jsonl
 from ._json_api import json_dumps, json_loads, is_json_serializable
 from ._msgpack_api import read_msgpack, write_msgpack, msgpack_dumps, msgpack_loads
 from ._pickle_api import pickle_dumps, pickle_loads
+from .about import __version__
