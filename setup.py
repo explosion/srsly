adc359ab9e25b42a56ebf79e5af5d4f54da1851b
Ines Montani <ines@ines.io>
2020-01-01 13:53:49 +0100

    Modernize setup and compilation

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
-import os
-import subprocess
 import sys
-import contextlib
 from distutils.command.build_ext import build_ext
 from distutils.sysconfig import get_python_inc
 from setuptools import Extension, setup, find_packages
 from pathlib import Path
+from Cython.Build import cythonize
+from Cython.Compiler import Options
+
+
+# Preserve `__doc__` on functions and classes
+# http://docs.cython.org/en/latest/src/userguide/source_files_and_compilation.html#compiler-options
+Options.docstrings = True
 
 
 PACKAGE_DATA = {"": ["*.pyx", "*.pxd", "*.c", "*.h"]}
@@ -16,6 +20,11 @@ COMPILE_OPTIONS = {
     "msvc": ["/Ox", "/EHsc"],
     "mingw32": ["-O2", "-Wno-strict-prototypes", "-Wno-unused-function"],
     "other": ["-O2", "-Wno-strict-prototypes", "-Wno-unused-function"],
+}
+COMPILER_DIRECTIVES = {
+    "language_level": -3,
+    "embedsignature": True,
+    "annotation_typing": False,
 }
 LINK_OPTIONS = {"msvc": [], "mingw32": [], "other": ["-lstdc++", "-lm"]}
 
@@ -30,11 +39,13 @@ else:
 # http://stackoverflow.com/questions/724664/python-distutils-how-to-get-a-compiler-that-is-going-to-be-used
 class build_ext_options:
     def build_options(self):
+        # if hasattr(self.compiler, "initialize"):
+        #     self.compiler.initialize()
+        # self.compiler.platform = sys.platform[:6]
         for e in self.extensions:
             e.extra_compile_args += COMPILE_OPTIONS.get(
                 self.compiler.compiler_type, COMPILE_OPTIONS["other"]
             )
-        for e in self.extensions:
             e.extra_link_args += LINK_OPTIONS.get(
                 self.compiler.compiler_type, LINK_OPTIONS["other"]
             )
@@ -46,33 +57,16 @@ class build_ext_subclass(build_ext, build_ext_options):
         build_ext.build_extensions(self)
 
 
-def generate_cython(root, source):
-    print("Cythonizing sources")
-    script = root / "bin" / "cythonize.py"
-    p = subprocess.call([sys.executable, str(script), source], env=os.environ)
-    if p != 0:
-        raise RuntimeError("Running cythonize failed")
-
-
 def clean(path):
+    n_cleaned = 0
     for name in MOD_NAMES:
         name = name.replace(".", "/")
         for ext in ["so", "html", "cpp", "c"]:
             file_path = path / f"{name}.{ext}"
             if file_path.exists():
                 file_path.unlink()
-
-
-@contextlib.contextmanager
-def chdir(new_dir):
-    old_dir = os.getcwd()
-    try:
-        os.chdir(new_dir)
-        sys.path.insert(0, new_dir)
-        yield
-    finally:
-        del sys.path[0]
-        os.chdir(old_dir)
+                n_cleaned += 1
+    print(f"Cleaned {n_cleaned} files")
 
 
 def setup_package():
@@ -81,64 +75,48 @@ def setup_package():
     if len(sys.argv) > 1 and sys.argv[1] == "clean":
         return clean(root)
 
-    with chdir(root):
-        with (root / "srsly" / "about.py").open("r") as f:
-            about = {}
-            exec(f.read(), about)
+    with (root / "srsly" / "about.py").open("r") as f:
+        about = {}
+        exec(f.read(), about)
 
-        include_dirs = [get_python_inc(plat_specific=True), ".", root / "include"]
-
-        ext_modules = []
-        for mod_name in MOD_NAMES:
-            mod_path = mod_name.replace(".", "/") + ".cpp"
-            extra_link_args = []
-            extra_compile_args = []
-            # ???
-            # Imported from patch from @mikepb
-            # See Issue #267. Running blind here...
-            if sys.platform == "darwin":
-                dylib_path = [".." for _ in range(mod_name.count("."))]
-                dylib_path = "/".join(dylib_path)
-                dylib_path = "@loader_path/%s/srsly/platform/darwin/lib" % dylib_path
-                extra_link_args.append("-Wl,-rpath,%s" % dylib_path)
-            ext_modules.append(
-                Extension(
-                    mod_name,
-                    [mod_path],
-                    language="c++",
-                    include_dirs=include_dirs,
-                    extra_link_args=extra_link_args,
-                    define_macros=macros,
-                    extra_compile_args=extra_compile_args,
-                )
-            )
-
+    include_dirs = [get_python_inc(plat_specific=True), "."]
+    ext_modules = []
+    for name in MOD_NAMES:
+        mod_path = name.replace(".", "/") + ".pyx"
         ext_modules.append(
             Extension(
-                "srsly.ujson.ujson",
-                sources=[
-                    "./srsly/ujson/ujson.c",
-                    "./srsly/ujson/objToJSON.c",
-                    "./srsly/ujson/JSONtoObj.c",
-                    "./srsly/ujson/lib/ultrajsonenc.c",
-                    "./srsly/ujson/lib/ultrajsondec.c",
-                ],
-                include_dirs=["./srsly/ujson", "./srsly/ujson/lib"],
-                extra_compile_args=["-D_GNU_SOURCE"],
+                name,
+                [mod_path],
+                language="c++",
+                include_dirs=include_dirs,
+                define_macros=macros,
             )
         )
-
-        if not (root / "PKG-INFO").exists():  # not source release
-            generate_cython(root, "srsly")
-
-        setup(
-            name="srsly",
-            packages=PACKAGES,
-            package_data=PACKAGE_DATA,
-            version=about["__version__"],
-            ext_modules=ext_modules,
-            cmdclass={"build_ext": build_ext_subclass},
+    ext_modules.append(
+        Extension(
+            "srsly.ujson.ujson",
+            sources=[
+                "./srsly/ujson/ujson.c",
+                "./srsly/ujson/objToJSON.c",
+                "./srsly/ujson/JSONtoObj.c",
+                "./srsly/ujson/lib/ultrajsonenc.c",
+                "./srsly/ujson/lib/ultrajsondec.c",
+            ],
+            include_dirs=["./srsly/ujson", "./srsly/ujson/lib"],
+            extra_compile_args=["-D_GNU_SOURCE"],
         )
+    )
+    print("Cythonizing sources")
+    ext_modules = cythonize(ext_modules, compiler_directives=COMPILER_DIRECTIVES)
+
+    setup(
+        name="srsly",
+        packages=PACKAGES,
+        version=about["__version__"],
+        ext_modules=ext_modules,
+        cmdclass={"build_ext": build_ext_subclass},
+        package_data=PACKAGE_DATA,
+    )
 
 
 if __name__ == "__main__":
