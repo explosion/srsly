b01cf57635cb8a3eb9d8ccce8946d4f98b75415d
Ines Montani <ines@ines.io>
2020-06-21 00:17:08 +0200

    Experiment with newline setting in test

--- srsly/tests/ruamel_yaml/test_api_change.py
+++ srsly/tests/ruamel_yaml/test_api_change.py
@@ -202,7 +202,7 @@ class TestDumpLoadUnicode:
         yaml = YAML()
         text_dict = {"text": u"HELLO_WORLD©"}
         file_name = str(tmpdir) + "/tstFile.yaml"
-        yaml.dump(text_dict, open(file_name, "w", encoding="utf8"))
+        yaml.dump(text_dict, open(file_name, "w", encoding="utf8", newline="\n"))
         assert open(file_name, "rb").read().decode("utf-8") == u"text: HELLO_WORLD©\n"
 
     def test_read_unicode(self, tmpdir):
