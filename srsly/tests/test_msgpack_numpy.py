d8b93b944b01ed975878eac1868118412540b077
crusaderky <crusaderky@gmail.com>
2025-11-21 12:13:01 +0000

    Reduce diff to test_msgpack_numpy

--- srsly/tests/test_msgpack_numpy.py
+++ srsly/tests/test_msgpack_numpy.py
@@ -3,15 +3,47 @@ import pytest
 
 pytest.importorskip("numpy", reason="numpy is required for these tests")
 
-from srsly import msgpack_dumps, msgpack_loads
-import numpy as np
 from numpy.testing import assert_equal, assert_array_equal
+import numpy as np
+from srsly import msgpack_dumps, msgpack_loads, msgpack_decoders, msgpack_encoders
+
+
+class ThirdParty(object):
+    def __init__(self, foo=b"bar"):
+        self.foo = foo
+
+    def __eq__(self, other):
+        return isinstance(other, ThirdParty) and self.foo == other.foo
 
 
 class test_numpy_msgpack(TestCase):
+    def setUp(self):
+        msgpack_encoders.register("thirdparty", func=self.encode_thirdparty)
+        msgpack_decoders.register("thirdparty", func=self.decode_thirdparty)
+    
+    def tearDown(self):
+        msgpack_encoders.deregister("thirdparty")
+        msgpack_decoders.deregister("thirdparty")
+
     def encode_decode(self, x):
         x_enc = msgpack_dumps(x)
         return msgpack_loads(x_enc)
+
+    def encode_thirdparty(self, obj):
+        if isinstance(obj, ThirdParty):
+            return {b"__thirdparty__": True, b"foo": obj.foo}
+        return obj
+
+    def decode_thirdparty(self, obj):
+        if b"__thirdparty__" in obj:
+            return ThirdParty(foo=obj[b"foo"])
+        return obj
+
+    def test_bytes(self):
+        assert_equal(type(self.encode_decode(b"foo")), bytes)
+
+    def test_str(self):
+        assert_equal(type(self.encode_decode("foo")), str)
 
     def test_numpy_scalar_bool(self):
         x = np.bool_(True)
@@ -201,3 +233,9 @@ class test_numpy_msgpack(TestCase):
         x_rec = self.encode_decode(x)
         assert_array_equal(x, x_rec)
         assert_array_equal([type(e) for e in x], [type(e) for e in x_rec])
+
+    def test_chain(self):
+        x = ThirdParty(foo=b"test marshal/unmarshal")
+        x_rec = self.encode_decode(x)
+        self.assertEqual(x, x_rec)
+
